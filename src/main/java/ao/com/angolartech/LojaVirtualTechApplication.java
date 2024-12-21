aeebb5c6ecebf2ef2ef5b150136a81d2e5cf0087
Fernando Angolar <fernandoangolar581@gmail.com>
2024-12-21 09:21:25 +0100

    Configurações inicias do projecto

---
+++ src/main/java/ao/com/angolartech/LojaVirtualTechApplication.java
@@ -0,0 +1,13 @@
+package ao.com.angolartech;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LojaVirtualTechApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LojaVirtualTechApplication.class, args);
+	}
+
+}
